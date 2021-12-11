fbe4817415a90934f9e10047005803c5504492dc
Victor Mafra <32077156+MafraV@users.noreply.github.com>
2021-12-10 23:14:20 -0300

    Update 3_alter_types.sql

--- Teste_3/3_alter_types.sql
+++ Teste_3/3_alter_types.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "Despesas" ALTER COLUMN "DATA" TYPE date USING ("DATA"::date);
-ALTER TABLE "Despesas" ALTER COLUMN "VL_SALDO_FINAL" TYPE numeric USING replace("VL_SALDO_FINAL",',','.')::numeric;
-ALTER TABLE "Despesas" ALTER COLUMN "REG_ANS" TYPE numeric USING ("REG_ANS"::numeric);
-ALTER TABLE "Registro" ALTER COLUMN "Registro_ANS" TYPE numeric USING replace("Registro_ANS",'"',' ')::numeric;
+ALTER TABLE "Despesas" ALTER COLUMN "DATA" TYPE date USING ("DATA"::date); /*Changes the "DATA" column from the "Despesas" table to date type*/
+ALTER TABLE "Despesas" ALTER COLUMN "VL_SALDO_FINAL" TYPE numeric USING replace("VL_SALDO_FINAL",',','.')::numeric; /*Changes the "VL_SALDO_FINAL" column from the "Despesas" table to numeric type*/
+ALTER TABLE "Despesas" ALTER COLUMN "REG_ANS" TYPE numeric USING ("REG_ANS"::numeric); /*Changes the "REG_ANS" column from the "Despesas" table to numeric type*/
+ALTER TABLE "Registro" ALTER COLUMN "Registro_ANS" TYPE numeric USING replace("Registro_ANS",'"',' ')::numeric; /*Changes the "Registro_ANS" column from the "Registro" table to numeric type*/
