8972454a00a0d524c7f409e77d2f27810e3843a7
Victor Mafra <32077156+MafraV@users.noreply.github.com>
2021-12-10 23:12:22 -0300

    Update 2_load_query.sql

--- Teste_3/2_load_query.sql
+++ Teste_3/2_load_query.sql
@@ -1,8 +1,8 @@
-COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/1T2020.csv' WITH DELIMITER ';' csv header encoding 'windows-1251';
-COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/2T2020.csv' WITH DELIMITER ';' csv header encoding 'windows-1251';
-COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/3T2020.csv' WITH DELIMITER ';' csv header encoding 'windows-1251';
-COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/4T2020.csv' WITH DELIMITER ';' csv header encoding 'windows-1251';
-COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/1T2021.csv' WITH DELIMITER ';' csv header encoding 'windows-1251';
-COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/2T2021.csv' WITH DELIMITER ';' csv header encoding 'windows-1251';
-COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/3T2021.csv' WITH DELIMITER ';' csv header encoding 'windows-1251';
-COPY "Registro" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/Relatorio_cadop.csv' WITH DELIMITER ';' quote E'\b' csv header encoding 'windows-1251';
+COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/1T2020.csv' WITH DELIMITER ';' csv header encoding 'windows-1251'; /*Loads the 1T2020.csv to the "Despesas" table*/
+COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/2T2020.csv' WITH DELIMITER ';' csv header encoding 'windows-1251'; /*Loads the 2T2020.csv to the "Despesas" table*/
+COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/3T2020.csv' WITH DELIMITER ';' csv header encoding 'windows-1251'; /*Loads the 3T2020.csv to the "Despesas" table*/
+COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/4T2020.csv' WITH DELIMITER ';' csv header encoding 'windows-1251'; /*Loads the 4T2020.csv to the "Despesas" table*/
+COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/1T2021.csv' WITH DELIMITER ';' csv header encoding 'windows-1251'; /*Loads the 1T2021.csv to the "Despesas" table*/
+COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/2T2021.csv' WITH DELIMITER ';' csv header encoding 'windows-1251'; /*Loads the 2T2021.csv to the "Despesas" table*/
+COPY "Despesas" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/3T2021.csv' WITH DELIMITER ';' csv header encoding 'windows-1251'; /*Loads the 3T2021.csv to the "Despesas" table*/
+COPY "Registro" FROM 'C:\Codes\Estagio - Desafios\Teste 3\CSVs/Relatorio_cadop.csv' WITH DELIMITER ';' quote E'\b' csv header encoding 'windows-1251'; /*Loads the Relatorio_cadop.csv to the "Registro" table*/
